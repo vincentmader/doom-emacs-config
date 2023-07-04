926d14916e3d48a132593bf2a700c7cf9cc151ad
Vincent C. Mader <vincent.mader@icloud.com>
2023-07-04 12:42:50 +0200

    fixed script resposible for linking custom theme

--- bin/setup_custom_theme.sh
+++ bin/setup_custom_theme.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-PATH_TO_THEME="$XDG_CONFIG_HOME/emacs/.local/straight/build-28.2/doom-themes/doom-solarized-dark-theme.el"
-rm $PATH_TO_THEME 2> /dev/null
-ln -s $XDG_CONFIG_HOME/doom/lisp/themes/solarized-custom.el $PATH_TO_THEME
+# Get version of installed Emacs.
+version="$(emacs --version)"
+version=(${version// / })
+version="${version[2]}"
+
+# Remove current version of `doom-solarized-dark-theme.el`.
+PATH_TO_EMACS="$XDG_CONFIG_HOME/emacs"
+PATH_TO_BUILD="$PATH_TO_EMACS/.local/straight/build-$version"
+PATH_TO_THEME="$PATH_TO_BUILD/doom-themes/doom-solarized-dark-theme.el"
+rm "$PATH_TO_THEME" 2> /dev/null
+
+# Link my `solarized-custom.el` theme.
+ln -s "$XDG_CONFIG_HOME/emacs-doom/lisp/themes/solarized-custom.el" "$PATH_TO_THEME"

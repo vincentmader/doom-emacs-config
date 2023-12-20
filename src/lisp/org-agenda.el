d5ed282ccc0a6e9d761eccf426b1e7c6aa54f6d5
Vincent C. Mader <vincent.mader@icloud.com>
2023-12-20 02:00:35 +0100

    Add agenda files

--- src/lisp/org-agenda.el
+++ src/lisp/org-agenda.el
@@ -27,16 +27,23 @@
 ; They are used in my "make-org-agenda" repo to auto-generate the list below.
 ; MARKER_START_ORG_AGENDA_FILE_LIST
     "/Users/vinc/org/project/active/setup_shared_data_partition_for_macos_and_asahi/Setup shared data partition for macOS and Asahi.org"
+    "/Users/vinc/org/project/active/develop_app_barWürfeln/Develop app barWürfeln.org"
+    "/Users/vinc/org/project/active/configure_hetzner_server/Configure Hetzner server.org"
+    "/Users/vinc/org/project/active/work_as_stavo_at_dpsg/Work as StaVo at DPSG.org"
     "/Users/vinc/org/project/active/develop_website_chronos/chronos.org"
     "/Users/vinc/org/project/active/develop_website_chronos/Develop website chronos.org"
     "/Users/vinc/org/project/active/develop_website_mader.xyz/Develop website mader.xyz.org"
+    "/Users/vinc/org/project/active/develop_tui-app_waslosamhaus/Configure WIFI at Harthauser Str. 119.org"
     "/Users/vinc/org/project/active/write_list_of_things_to_achieve_during_my_time_as_stavo/Write list of things to achieve during my time as StaVo.org"
     "/Users/vinc/org/project/active/work_as_aushilfe_at_gastroevents/GastroEvents.org"
     "/Users/vinc/org/project/active/work_as_aushilfe_at_gastroevents/Work as Aushilfe at GastroEvents.org"
     "/Users/vinc/org/project/active/work_as_leiter_at_dpsg/Work as Leiter at DPSG.org"
+    "/Users/vinc/org/project/active/complete_tutorial_on_swiftui_by_apple/Complete tutorial on SwiftUI by Apple.org"
     "/Users/vinc/org/project/active/configure_m1_macbook_pro/Configure M1 MacBook Pro.org"
     "/Users/vinc/org/project/active/write_master_thesis/Write Master Thesis.org"
-    "/Users/vinc/org/project/active/write_satzung_for_pfadfinder_ring_ulm/Write Satzung for Pfadfinder-Ring Ulm.org"
+    "/Users/vinc/org/project/active/write_rede_zur_lage_der_nation_2023_for_dpsg_ul_soe/Write \"Rede zur Lage der Nation 2023\" for DPSG Ul-Soe.org"
+    "/Users/vinc/org/project/active/write_rede_zur_lage_der_nation_2023_for_dpsg_ul_soe/Jahresplanung.org"
+    "/Users/vinc/org/project/active/develop_simulations_from_computational_physics/Develop simulations from computational physics.org"
     "/Users/vinc/org/chore/keep_order_at_home/Keep order at Home.org"
     "/Users/vinc/org/chore/implement_gtd_system/Implement GTD system.org"
     "/Users/vinc/org/chore/implement_gtd_system/GTD.org"
@@ -48,4 +55,3 @@
 ; MARKER_END_ORG_AGENDA_FILE_LIST
   )))
 
-; }}}
